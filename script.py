8bc2560d1ef1afa23a95b469e6bdc5e9a10f6928
Lautaropj <lautaroalderete68@gmail.com>
2025-08-04 12:49:17 -0300

    Update script.py

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import pyodbc
 
+#Modifique de acuerdo a su Server y DB
 conn = pyodbc.connect(
         'DRIVER={ODBC Driver 18 for SQL Server};'
         'SERVER=sv_name;'
@@ -19,4 +20,5 @@ productos = pd.read_csv("productos.csv", sep=';')
 
 merged = clientes.merge(productos, on="cod_producto")
 merged.to_csv("clientes_y_productos_merged.csv", index=False)
+
 
